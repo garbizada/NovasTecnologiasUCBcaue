9556ab4e9a2d85ec14e21040a10a3f3486e1ac1b
CAUÊ JUSTEN GARBI <caue.garbi@a.ucb.br>
2025-03-07 09:08:36 -0300

    primeira atividade

---
+++ variavesOperadores.js
@@ -0,0 +1,19 @@
+// Declaração de variáveis numéricas
+let num_A = 10;
+let num_B = 5;
+
+// Soma
+let resultado_soma = num_A + num_B;
+console.log(`Resultado da soma entre ${num_A} e ${num_B} é ${resultado_soma}`);
+
+// Subtração
+let resultado_subtracao = num_A - num_B;
+console.log(`Resultado da subtração entre ${num_A} e ${num_B} é ${resultado_subtracao}`);
+
+// Multiplicação
+let resultado_multiplicacao = num_A * num_B;
+console.log(`Resultado da multiplicação entre ${num_A} e ${num_B} é ${resultado_multiplicacao}`);
+
+// Divisão
+let resultado_divisao = num_A / num_B;
+console.log(`Resultado da divisão entre ${num_A} e ${num_B} é ${resultado_divisao}`);
