dce5666b1248ff317a122f5b8f182b31a3f0ea6e
garbizada <caue.justen@hotmail.com>
2025-03-14 23:46:27 -0300

    Feito as atividades

--- lista2.py
+++
@@ -1,9 +0,0 @@
-def separate_digits():
-    number = input("Digite um número de cinco dígitos: ")
-    if len(number) == 5 and number.isdigit():
-        print("   ".join(number))
-    else:
-        print("Por favor, insira exatamente cinco dígitos.")
-
-if __name__ == "__main__":
-    separate_digits()
