792b14193d4ce3d84b10c87115591bb0683b933c
garbizada <caue.justen@hotmail.com>
2025-03-14 23:02:51 -0300

    Lista de Atividades

---
+++ lista1.py
@@ -0,0 +1,38 @@
+def draw_box():
+    print("*****")
+    print("*   *")
+    print("*   *")
+    print("*   *")
+    print("*****")
+
+def draw_oval():
+    print("  ***  ")
+    print(" *   * ")
+    print("*     *")
+    print(" *   * ")
+    print("  ***  ")
+
+def draw_arrow():
+    print("   *   ")
+    print("  ***  ")
+    print(" ***** ")
+    print("   *   ")
+    print("   *   ")
+    print("   *   ")
+    print("   *   ")
+
+def draw_diamond():
+    print("   *   ")
+    print("  * *  ")
+    print(" *   * ")
+    print("  * *  ")
+    print("   *   ")
+
+if __name__ == "__main__":
+    draw_box()
+    print()
+    draw_oval()
+    print()
+    draw_arrow()
+    print()
+    draw_diamond()
